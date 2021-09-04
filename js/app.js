99eff15b950675d4f3b9ffe9eab012266a1fcc3f
CoderMahabub <codermahabub@gmail.com>
2021-09-05 04:14:10 +0600

    Weather API Intregrated

--- js/app.js
+++ js/app.js
@@ -2,12 +2,28 @@ const API_KEY = `9b4c06168764b0cd99e3031a1c698efb`;
 
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data))
+    document.getElementById('city-name').value = '';
 }
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
 
 const displayTemperature = temperature => {
     console.log(temperature);
+
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+
+    // Set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+
 }
